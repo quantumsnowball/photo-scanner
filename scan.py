3ca9604f01df6a48de609c550f71c74c7615e945
Quantum Snowball <quantum.snowball@gmail.com>
2023-11-07 02:30:24 +0800

    rft: modularized

--- scan.py
+++ scan.py
@@ -1,10 +1,24 @@
 import subprocess
 
 
-EXECUTABLE = r'/mnt/c/Program Files/NAPS2/NAPS2.Console.exe'
-PROFILE = 'fast'
+NAME = '.raw.jpg'
+EXE = r'/mnt/c/Program Files/NAPS2/NAPS2.Console.exe'
+PROFILE = 'jpg300'
 
-output = input('Output file name? ')
-cmd = [EXECUTABLE, '-p', PROFILE, '-o', output, ]
 
-result = subprocess.run(cmd)
+def scan(savename: str,
+         exe: str = EXE,
+         profile: str = PROFILE,) -> None:
+    '''
+    scan using NAPS2 console executable
+    '''
+    # cmd
+    cmd = [exe, '-p', profile, '-o', savename, '--progress', '--force']
+
+    # run
+    _ = subprocess.run(cmd)
+
+
+if __name__ == '__main__':
+    name = input('Output file name? ')
+    scan(name)
