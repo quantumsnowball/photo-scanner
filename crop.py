eacc214613f8006baaef66cb2b43cffee333a5cf
Quantum Snowball <quantum.snowball@gmail.com>
2023-11-08 03:21:55 +0800

    step: show image in viewer

--- crop.py
+++ crop.py
@@ -1,23 +1,28 @@
 from pathlib import Path
-from PIL import Image
+from PIL import Image, ImageDraw
 import numpy as np
 
 
 def read_image_array(name: Path,
-                     rotation: int = 270) -> np.ndarray:
+                     rotation: int = 270) -> Image.Image:
     image = Image.open(name)
     rotated = image.rotate(rotation, expand=True)
-    arr = np.array(rotated)
-    return arr
+    return rotated
+
+
+def preview_crop(image) -> None:
+    draw = ImageDraw.Draw(image)
+    image.show()
 
 
 if __name__ == '__main__':
     # read the raw image
     raw = read_image_array(Path('.lab/raw.jpg'))
     # read the user coordinate and width height info
-    import matplotlib.pyplot as plt
-    plt.imshow(raw)
-    plt.show()
+    preview_crop(raw)
+    # import matplotlib.pyplot as plt
+    # plt.imshow(np.array(raw))
+    # plt.show()
     breakpoint()
     # display the preview of the crop
     # crop and save the photos
