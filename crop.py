883c1131f8500d724edcdfbb8e9a27d02e4ca6ff
Quantum Snowball <quantum.snowball@gmail.com>
2023-11-08 03:46:03 +0800

    step: draw all lines demo

--- crop.py
+++ crop.py
@@ -2,6 +2,12 @@ from pathlib import Path
 from PIL import Image, ImageDraw
 
 
+X = (700, 2400)
+Y = (120, 1400)
+W = 1550
+H = 1080
+
+
 def read_image_array(name: Path,
                      rotation: int = 270) -> Image.Image:
     # open
@@ -13,13 +19,19 @@ def read_image_array(name: Path,
 
 
 def preview_crop(image: Image.Image,
-                 fill: tuple[int, int, int] = (0, 255, 0, ),
                  width: int = 5) -> None:
     draw = ImageDraw.Draw(image)
     # Define the coordinates of the line
     line_coordinates = [(1000, 0), (1000, 1000)]
     # Draw the line on the image
-    draw.line(line_coordinates, fill=fill, width=width)
+    # draw.line(line_coordinates, fill=fill, width=width)
+    for x in X:
+        draw.line([(x, 0), (x, image.height)], fill='red', width=width)
+        draw.line([(x+W, 0), (x+W, image.height)], fill='green', width=width)
+    for y in Y:
+        draw.line([(0, y), (image.width, y)], fill='red', width=width)
+        draw.line([(0, y+H), (image.width, y+H)], fill='green', width=width)
+
     # show
     image.show()
 
@@ -32,6 +44,5 @@ if __name__ == '__main__':
     # import matplotlib.pyplot as plt
     # plt.imshow(np.array(raw))
     # plt.show()
-    breakpoint()
     # display the preview of the crop
     # crop and save the photos
