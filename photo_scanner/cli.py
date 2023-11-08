7713d5aabb018aa1104e631a6ecd5711999cc8d2
Quantum Snowball <quantum.snowball@gmail.com>
2023-11-08 21:05:45 +0800

    feat: command and sub-command

--- photo_scanner/cli.py
+++ photo_scanner/cli.py
@@ -1,14 +1,18 @@
 import click
 
 
-@click.group()
-def photo_scanner() -> None:
-    pass
+@click.group(invoke_without_command=True)
+@click.pass_context
+def photo_scanner(ctx: click.Context) -> None:
+    # ignore subcommand call
+    if ctx.invoked_subcommand is not None:
+        return
+
+    # do default operations
+    click.echo(f'`photo-scanner` is called')
 
 
-@click.command
-def scan() -> None:
-    print('hello click photo-scanner')
-
-
-photo_scanner.add_command(scan)
+@photo_scanner.command()
+def preview() -> None:
+    # do subcommand operations
+    click.echo(f'`photo-scanner preview` is called')
