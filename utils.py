d7726cf987065231e6bb7b07b6dcb01df88d05a3
Quantum Snowball <quantum.snowball@gmail.com>
2023-11-08 16:20:37 +0800

    step: yaml config file parsing

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 from typing import Literal
 from PIL import Image
+from dataclasses import dataclass
+import yaml
 
 
 def save_images(images: list[Image.Image],
@@ -10,3 +12,23 @@ def save_images(images: list[Image.Image],
                 ext: Literal['jpg', 'png'] = 'jpg') -> None:
     for i, image in enumerate(images):
         image.save(outdir / f'{prefix}_{i}.{ext}')
+
+
+@dataclass
+class CropLocation:
+    x: int
+    y: int
+    width: int
+    height: int
+
+
+def read_cropping_config_yaml(path: Path) -> CropLocation:
+    with open(path) as file:
+        values = yaml.safe_load(file)
+        crop_config = CropLocation(**values)
+        breakpoint()
+        return crop_config
+
+
+if __name__ == '__main__':
+    read_cropping_config_yaml(Path('config.yaml'))
